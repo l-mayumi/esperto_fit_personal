6c19ed998bac8f0fe16a03f734e303d57555f5c2
Leticia Mayumi <hashizumemay@gmail.com>
2019-10-13 21:40:03 -0300

    Unit tests added

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
+    describe 'type: customer' do
+        it 'must have a document' do
+            account = create(:account)
+            expect(account).to be_valid
+            account.document = nil
+            expect(account).to_not be_valid
+        end
+    end
+
+    describe 'type: personal' do
+        it 'must have a document' do
+            personal = create(:personal)
+            expect(personal).to be_valid
+            personal.document = nil
+            expect(personal).to_not be_valid
+        end
+
+
+        it 'must have a unique document' do
+            personal = create(:personal, document:'32345678900')
+            other_personal = create(:personal, document: '32345678901')
+            expect(personal).to be_valid
+            personal.document = '32345678901'
+            expect(personal).to_not be_valid
+        end
+    end
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Enrollment, type: :model do
+    describe 'registers the right' do
+        it 'customer' do
+            account = create(:account)
+            unit = create(:unit)
+            enrollment = Enrollment.create(account: account, unit: unit)
+
+            expect(enrollment.account).to eq account
+        end
+
+        it 'unit' do
+            account = create(:account)
+            unit = create(:unit)
+            another_unit = create(:unit)
+            enrollment = Enrollment.create(account: account, unit: unit)
+
+            expect(enrollment.unit).to eq unit
+            enrollment.unit = another_unit
+            expect(enrollment.unit).to eq another_unit
+        end
+    end
 
 end
